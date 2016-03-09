eb49cc1d944cf4bb56f0422c7fb83e63ac388538
congerfish <congerfish@gmail.com>
2016-03-09 16:47:21 +0800

    Update cachematrix.R
    Add code content

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,44 @@
 ## functions do
 
 ## Write a short comment describing this function
+## original matrix will be instored in x
+## inverted matrix will be instored in mi
 
 makeCacheMatrix <- function(x = matrix()) {
+  mi <- NULL
+  
+  setMatrix <- function(m){
+    x <<- m
+    mi <<- NULL
+  }
+  
+  getMatrix <- function() x
+  
+  getInvert <- function() mi
 
+  setInvert <- function(invert) mi <<- invert
+  
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvert = setInvert,
+       getInvert = getInvert)
 }
 
 
 ## Write a short comment describing this function
+## check if matrix has been inverted before first
+## if true, return the inverted matrix directly
+## if false, inverse the matrix, store it in x, and return it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mi <- x$getInvert()
+  if(!is.null(mi)){
+    message("getting inverse matrix")
+    return (mi)
+  }
+  m <- x$getMatrix()
+  mi <- solve(m)
+  x$setInvert(mi)
+  mi
 }
